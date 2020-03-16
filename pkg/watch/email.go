aa4b8efe4c4bde3ae85582e31950ccba5ef08f5e
Jason Kulatunga <jason@thesparktree.com>
2020-03-15 22:26:20 -0700

    remove todo

--- pkg/watch/email.go
+++ pkg/watch/email.go
@@ -85,7 +85,7 @@ func (ew *EmailWatcher) batchProcessMessages(c *client.Client) {
 		//todo publish/generate events for stored documents
 		ew.generateEvent()
 
-		//todo, delete messages
+		//delete messages
 		ew.deleteMessages(c, seqset)
 	}
 
